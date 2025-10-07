8293dfb59f4c7247d39f43a2ef889dd1ce183173
bric12369 <barryrichards05@gmail.com>
2025-10-07 18:45:38 +0100

    created more tables and added foreign keys

--- db/create-all-tables.js
+++ db/create-all-tables.js
@@ -3,14 +3,34 @@ const db = require('./connection')
 const createAllTables = async () => {
 
     await db.query(`DROP TABLE IF EXISTS books`)
-    
+    await db.query(`DROP TABLE IF EXISTS authors`)
+    await db.query(`DROP TABLE IF EXISTS genres`)
+    await db.query(`DROP TABLE IF EXISTS conditions`)
+
+    await db.query(`CREATE TABLE authors(
+        author_id SERIAL PRIMARY KEY,
+        first_name VARCHAR NOT NULL,
+        surname VARCHAR NOT NULL,
+        avatar VARCHAR
+        )`)
+
+    await db.query(`CREATE TABLE genres(
+        genre VARCHAR PRIMARY KEY,
+        description TEXT NOT NULL
+        )`)
+
+    await db.query(`CREATE TABLE conditions(
+        condition VARCHAR PRIMARY KEY,
+        description TEXT NOT NULL
+        )`)
+
     await db.query(`CREATE TABLE books(
         book_id SERIAL PRIMARY KEY,
         book_name VARCHAR NOT NULL,
         description TEXT NOT NULL,
-        author_id INT NOT NULL,
-        genre VARCHAR NOT NULL,
-        condition VARCHAR NOT NULL,
+        author_id INT NOT NULL REFERENCES authors(author_id),
+        genre VARCHAR NOT NULL REFERENCES genres(genre),
+        condition VARCHAR NOT NULL REFERENCES conditions(condition),
         price DECIMAL(4, 2) NOT NULL
         )`)
 }
