5328a37bcb5d03721c488bf80f8c7a69abdf389e
bric12369 <barryrichards05@gmail.com>
2025-10-08 14:12:33 +0100

    created index to group exports

---
+++ db/data/test/index.js
@@ -0,0 +1,4 @@
+exports.authorsData = require('./authors.json')
+exports.booksData = require('./books.json')
+exports.conditionsData = require('./conditions.json')
+exports.genresData = require('./genres.json')
