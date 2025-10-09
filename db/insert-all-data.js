c5128ce24448d7d54140a9f7e5bec856ffee5d8b
bric12369 <barryrichards05@gmail.com>
2025-10-09 17:00:36 +0100

    inserted into genres table

--- db/insert-all-data.js
+++ db/insert-all-data.js
@@ -1,6 +1,6 @@
 const db = require('./connection')
 const pgFormat = require('pg-format')
-const { authorsData } = require('./data/test')
+const { authorsData, booksData, conditionsData, genresData } = require('./data/test')
 
 const insertAllData = async () => {
 
@@ -8,10 +8,21 @@ const insertAllData = async () => {
         return [a.first_name, a.surname, a.avatar]
     })
 
+    const genresValues = genresData.map(g => {
+        return [g.genre, g.description]
+    })
+
     await db.query(
         pgFormat(
             `INSERT INTO authors (first_name, surname, avatar) VALUES %L`,
             authorValues)
+    )
+
+    await db.query(
+        pgFormat(
+            `INSERT INTO genres (genre, description) VALUES %L`,
+            genresValues
+        )
     )
 }
 
