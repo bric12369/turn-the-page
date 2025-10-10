9b07962af68fb52c866fd52b10aec23727f1bc80
bric12369 <barryrichards05@gmail.com>
2025-10-10 16:00:51 +0100

    moved app.listen to server file

--- app.js
+++ app.js
@@ -2,14 +2,8 @@ const express = require('express')
 
 const app = express()
 
-const port = 3000
-
 app.get('/', async (req, res) => {
     res.send('Hello!')
-})
-
-app.listen(port, () => {
-    console.log(`Listening on PORT: ${port}`)
 })
 
 module.exports = app
